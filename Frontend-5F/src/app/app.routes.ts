6ef1e02272cc30aab15b0e4eac75100d3ec28fc0
mattaLeo <159021786+mattaLeo@users.noreply.github.com>
2026-02-26 13:16:40 +0000

    routes

--- Frontend-5F/src/app/app.routes.ts
+++ Frontend-5F/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { Automobilista } from './automobilista/automobilista';
 import { Perito } from './perito/perito';
 import { Assicurazione } from './assicurazione/assicurazione';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
 
 export const routes: Routes = [
     {path: "automobilista", component: Automobilista},
     {path: "perito", component: Perito},
-    {path: "assicurazione", component: Assicurazione}
+    {path: "assicurazione", component: Assicurazione},
+    {path: "signin", component: Login},
+    {path: "signup", component: Signup}
 ];
