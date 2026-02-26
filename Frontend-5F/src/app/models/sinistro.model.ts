f7d6282b6828b4569c2df6c382f92491aa9bc09f
mattaLeo <159021786+mattaLeo@users.noreply.github.com>
2026-02-26 20:10:16 +0000

    chiamate http sinistri v1

--- Frontend-5F/src/app/models/sinistro.model.ts
+++ Frontend-5F/src/app/models/sinistro.model.ts
@@ -1,7 +1,7 @@
 export interface sinistro{
     id_automobilista: number
     targa: string
-    data: Date
+    data_evento: Date
     descrizione: string
     stato: "APERTO" | "CHIUSO" | "IN ANALISI" | string
     data_creazione: Date
